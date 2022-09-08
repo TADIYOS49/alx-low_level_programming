13f5b9cc6aa7f5d81af61735035f6d6b65099e45
tadiyos <tadiyosbanti@gmail.com>
2022-09-08 21:42:54 +0300

    update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,8 @@
 /**
  * main - Entry point
  *
- * Description: prints "with proper grammar, but the outcome is a piece of art,\n"
+ * Description: prints "with proper grammar, but the outcome is a piece of
+ * art,"
  *
  * Return : Always 0
  */
