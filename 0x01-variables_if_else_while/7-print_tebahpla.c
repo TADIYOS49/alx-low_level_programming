1c34258b6cdab8cac5cd88493750ab0e758e0993
tadiyos <tadiyosbanti@gmail.com>
2022-09-09 16:35:25 +0300

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print lowercase alphabet
+ *
+ * Description: print lowercase reverse
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{
+	int i = 129;
+
+	while (i >= 97)
+	{
+		putchar(i);
+		i--;
+	}
+	putchar(10);
+	return (0);
+}
