07e640e266c6e868a7386923d27109a800d16849
tadiyos <tadiyosbanti@gmail.com>
2022-09-09 15:55:13 +0300

    iteration

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print iteration
+ *
+ * Description: print from 0 to 10
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf(i);
+	}
+	putchar(10);
+	return (0);
+}
