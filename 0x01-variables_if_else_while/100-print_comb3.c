e5837460ff0c380c22407ea76a16676595e278eb
tadiyos <tadiyosbanti@gmail.com>
2022-09-09 17:21:34 +0300

    combo

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print combo
+ *
+ * Description: print combo of numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	for (int i = 48; i < 58; i++)
+	{
+		for (int j = i+1; j < 58; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if (i != 57 && j != 58)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
