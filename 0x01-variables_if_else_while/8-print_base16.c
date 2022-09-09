76a8bdb4521d0f9762af7218f2310a10b9a1fc8f
tadiyos <tadiyosbanti@gmail.com>
2022-09-09 16:48:45 +0300

    hexa

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print the letters of the alphabet
+ *
+ * Description: print the letters of the alphabet except e, q
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+
+	i = 97;
+
+	while (i < 104)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar(10);
+	return (0);
+}
