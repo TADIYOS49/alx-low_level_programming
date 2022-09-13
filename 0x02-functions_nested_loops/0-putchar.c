6fcc6ec941f048f9b639b9b0d608f85abfcb13d0
Tadiyos <tadiyosbanti@gmail.com>
2022-09-13 23:32:28 +0300

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - display _putchar
+ *
+ * Description: display a string
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
