76eb16ce99e17ffe6da180f7492f6b07ce0a41d8
Tadiyos <tadiyosbanti@gmail.com>
2022-09-19 18:28:16 +0300

    string copy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcpy - string copy from src to destination.
+ * @dest: string
+ * @src: string
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len = strlen(src);
+	int i;
+
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
