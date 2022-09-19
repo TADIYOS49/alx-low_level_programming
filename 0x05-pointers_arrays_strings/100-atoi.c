0ed8c6bdf226c5bab79b47956eaa55fdab4a23f2
Tadiyos <tadiyosbanti@gmail.com>
2022-09-19 19:12:18 +0300

    string to integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,25 @@
+#include <string.h>
+#include "main.h"
+
+
+/**
+ * _atoi - string to integer
+ * @s: string
+ * Return: integer
+ */
+
+int _atoi(char *s)
+{
+	int len = strlen(s);
+	int i, sign, num;
+
+	for (i = 1; i < len; i++)
+	{
+		num = num * 10 + s[i];
+	}
+	if (s[0] == "-")
+	{
+		num = num * -1;
+	}
+	return (num);
+}
