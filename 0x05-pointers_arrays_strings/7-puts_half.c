e4c26379b0bb96b2b606f5db2a11a1864adfdb7e
Tadiyos <tadiyosbanti@gmail.com>
2022-09-19 15:23:24 +0300

    update

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,9 +13,9 @@ void puts_half(char *str)
 {
 	int len = strlen(str);
 	int i;
-	int n = len / 2;
+	int n = len - 1 / 2;
 
-	for (i = n; i <= len; i++)
+	for (i = n; i <= len - 1; i++)
 	{
 		_putchar(str[i]);
 	}
