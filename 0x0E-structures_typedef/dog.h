4ca82b5a0c37d30785cf06191941681606cebb6f
Tadiyos <tadiyosbanti@gmail.com>
2022-10-11 10:33:09 +0300

    readme and struct for do

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - best friend of humankind
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner's name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* !DOG_H */
