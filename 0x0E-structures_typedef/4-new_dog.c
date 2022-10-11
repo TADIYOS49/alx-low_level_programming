d4bf8bc5031fb68940b605e1554a23a85be0a6c8
Tadiyos <tadiyosbanti@gmail.com>
2022-10-11 12:17:26 +0300

    update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -23,6 +23,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (name1 == NULL)
 	{
 		free(name1);
+		free(dog);
 		return (NULL);
 	}
 
@@ -30,6 +31,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (owner1 == NULL)
 	{
 		free(owner1);
+		free(dog);
 		return (NULL);
 	}
 
