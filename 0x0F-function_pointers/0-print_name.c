365edf828b0c9640b66b14f17d9e5dcbfb671abe
Tadiyos <tadiyosbanti@gmail.com>
2022-10-12 08:43:57 +0300

    readme and print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name from a given name.
+ * @name: string that we need to display.
+ * @f: function that is going to be called from our function.
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f == NULL)
+		return;
+	f(name);
+}
